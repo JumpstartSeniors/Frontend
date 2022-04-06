6d192b6be39003dcef7936feda6d4d3028f3b3b1
Mohammed Al-Rasheed <mohammed.satar.alrasheed@gmail.com>
2022-04-06 01:26:10 -0400

    added like feature

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -1,67 +1,101 @@
-import React from 'react';
+import React from "react";
 import { useParams } from "react-router-dom";
-import { ChakraProvider, Grid, Box, Image, Link, Text } from '@chakra-ui/react'
-
+import { ChakraProvider, Grid, Box, Image, Link, Text } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { ChevronUpIcon, ChevronDownIcon } from "@chakra-ui/icons";
 // create a function that gets the current url
 function getCurrentUrl() {
-    let url = window.location.href;
-    let urlArray = url.split('/');
-    return urlArray[4];
+  let url = window.location.href;
+  let urlArray = url.split("/");
+  return urlArray[4];
 }
 
 class CourseNotes extends React.Component {
-    courseNotes = {
-        data: []
-    }
+  courseNotes = {
+    data: [],
+  };
 
+  componentDidMount() {
+    const url = "https://jumpstartbackendd.herokuapp.com/notes/" + getCurrentUrl();
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        this.courseNotes.data = data;
+        console.log(getCurrentUrl());
+        console.log(data);
+        this.forceUpdate();
+      });
+  }
 
-    componentDidMount() {
-        const url = 'https://jumpstartbackendd.herokuapp.com/notes/' + getCurrentUrl();
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                this.courseNotes.data = data;
-                console.log(getCurrentUrl());
-                console.log(data);
-                this.forceUpdate();
-            })
-    }
+  render() {
+    return (
+      <ChakraProvider>
+        <div mt={15}>
+          {this.courseNotes.data.map((note) => {
+            return (
+              // add border
+              <Box
+                borderWidth="1px"
+                borderColor="gray.200"
+                borderRadius="lg"
+                p={4}
+                display={{ md: "flex" }}
+              >
+                  <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+                <Link href={note.source}>
+                    <Text
+                      mt={1}
+                      display="block"
+                      fontSize="lg"
+                      lineHeight="normal"
+                      fontWeight="semibold"
+                    >
+                      {note.title}
+                    </Text>
 
-    render() {
-        return (
-            <ChakraProvider>
-                <div mt={15}>
-                    {this.courseNotes.data.map(note => {
-                        return (
-                            // add border
-                            <Link href={note.source} >
-                                <Box borderWidth="1px" borderColor="gray.200" borderRadius="lg" p={4} display={{ md: 'flex' }}>
-                                    <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
-                                        <Text
-                                            mt={1}
-                                            display='block'
-                                            fontSize='lg'
-                                            lineHeight='normal'
-                                            fontWeight='semibold'
-                                        >
-                                            {note.title}
-                                        </Text>
-
-                                        <Text mt={2} color='gray.500'>
-                                            Date Posted: {note.datePosted}
-                                        </Text>
-                                        <Text mt={2} color='gray.1000'>
-                                            {note.description}
-                                        </Text>
-                                    </Box>
-                                </Box>
-                            </Link>
-                        )
-                    })}
-                </div>
-            </ChakraProvider>
-        )
-    }
+                    <Text mt={2} color="gray.500">
+                      Date Posted: {note.datePosted}
+                    </Text>
+                    <Text mt={2} color="gray.1000">
+                      {note.description}
+                    </Text>
+                        </Link>
+                    <Text mt={2} color="gray.500">
+                        Likes: {note.likes}
+                        <Button
+                            variantColor="blue"
+                            variant="outline"
+                            ml={2}
+                            size="sm"
+                            icon={<ChevronUpIcon />}
+                            onClick={() => {
+                                const url = "https://jumpstartbackendd.herokuapp.com/notes/like/" + note._id;
+                                // make a post request to url
+                                fetch(url, {
+                                    method: "POST",
+                                    headers: {
+                                        "Content-Type": "application/json",
+                                    },
+                                })
+                                    .then((res) => res.json())
+                                    .then((data) => {
+                                        console.log(data);
+                                        this.forceUpdate();
+                                    });
+                                window.location.reload();
+                            }}
+                        >
+                            Upvote
+                        </Button>
+                    </Text>
+                  </Box>
+              </Box>
+            );
+          })}
+        </div>
+      </ChakraProvider>
+    );
+  }
 }
 
 export default CourseNotes;
