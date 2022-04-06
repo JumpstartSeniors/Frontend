bde966e5845eeba93a12e8468a7c7d6c5475d17b
Mohammed Al-Rasheed <mohammed.satar.alrasheed@gmail.com>
2022-04-06 13:08:23 -0400

    fixed reload issue

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -112,7 +112,8 @@ class CourseNotes extends React.Component {
                           .then((data) => {
                             console.log(data);
                             this.forceUpdate();
-                          });
+                            window.location.reload();
+                        });
                       }}
                     >
                       Upvote
